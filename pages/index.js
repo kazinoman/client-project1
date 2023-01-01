a6183e302480d639c537e52ca7607f19666edb29
abdullah al noman <kazinoman9999@gmail.com>
2023-01-01 21:14:44 +0600

    chg: header content change

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,27 @@
-import React from 'react'
+import React from "react";
 
-import { Box, Container, Typography } from '@mui/material'
+import { Box, Container, Typography } from "@mui/material";
 
-import SliderContent from '@/components/sliderContent'
-import SliderCom from '@/components/slider'
-import AllCard from '@/components/basicBenefits/allCard'
-import PromotionCard from '../components/promotionCard/promotionCard'
-import PopularPlacesCard from '@/components/PopularPlaces/popularPlaces'
-import HelpCenter from '@/components/helpCenter/helpCenter'
+import SliderContent from "@/components/sliderContent";
+import SliderCom from "@/components/slider";
+import AllCard from "@/components/basicBenefits/allCard";
+import PromotionCard from "../components/promotionCard/promotionCard";
+import PopularPlacesCard from "@/components/PopularPlaces/popularPlaces";
+import HelpCenter from "@/components/helpCenter/helpCenter";
 
 const Home = () => {
   return (
     <div>
       {/* slider and slider component */}
-      <Box sx={{ position: 'relative' }}>
+      <Box sx={{ position: "relative" }}>
         <Box
           sx={{
-            position: 'absolute',
+            position: "absolute",
             zIndex: 10,
-            bottom: '0',
-            height: '100%',
-            width: '100vw',
-            background: '#000000a1',
+            bottom: "0",
+            height: "100%",
+            width: "100vw",
+            background: "#000000a1",
           }}
         ></Box>
         <SliderContent />
@@ -41,7 +41,7 @@ const Home = () => {
       {/* Help center */}
       <HelpCenter />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- styles/theme.js
+++ styles/theme.js
@@ -1,103 +1,103 @@
-import { createTheme } from '@mui/material/styles'
+import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#18BABD',
+      main: "#18BABD",
     },
     secondary: {
-      main: '#3C4A6D',
+      main: "#3C4A6D",
     },
     buttonColor: {
-      main: '#9C2F86',
+      main: "#9C2F86",
     },
   },
   typography: {
-    fontFamily: 'Roboto, Lato, sans-serif',
+    fontFamily: "Roboto, Lato, sans-serif",
   },
-})
+});
 
 theme.typography.large = {
-  fontWeight: 'bold',
-  color: '#fff',
-  [theme.breakpoints.up('md')]: {
-    fontSize: '3rem',
+  fontWeight: "bold",
+  color: "#fff",
+  [theme.breakpoints.up("md")]: {
+    fontSize: "3rem",
   },
-  [theme.breakpoints.only('sm')]: {
-    fontSize: '2rem',
+  [theme.breakpoints.only("sm")]: {
+    fontSize: "2rem",
   },
-  [theme.breakpoints.down('sm')]: {
-    fontSize: '2rem',
+  [theme.breakpoints.down("sm")]: {
+    fontSize: "2rem",
   },
-}
+};
 theme.typography.title = {
-  fontWeight: '700',
-  color: '#000',
-  [theme.breakpoints.up('md')]: {
-    fontSize: '1.5rem',
+  fontWeight: "600",
+  color: "#000",
+  [theme.breakpoints.up("md")]: {
+    fontSize: "24px",
   },
-  [theme.breakpoints.only('sm')]: {
-    fontSize: '1rem',
+  [theme.breakpoints.only("sm")]: {
+    fontSize: "1rem",
   },
-  [theme.breakpoints.down('sm')]: {
-    fontSize: '1rem',
+  [theme.breakpoints.down("sm")]: {
+    fontSize: "1rem",
   },
-}
+};
 
 theme.typography.small = {
-  fontWeight: '400',
-  color: '#fff',
-  [theme.breakpoints.up('md')]: {
-    fontSize: '1.2rem',
+  fontWeight: "400",
+  color: "#fff",
+  [theme.breakpoints.up("md")]: {
+    fontSize: "1.2rem",
   },
-  [theme.breakpoints.only('md')]: {
-    fontSize: '1rem',
+  [theme.breakpoints.only("md")]: {
+    fontSize: "1rem",
   },
-  [theme.breakpoints.down('sm')]: {
-    fontSize: '1rem',
+  [theme.breakpoints.down("sm")]: {
+    fontSize: "1rem",
   },
-}
+};
 
 theme.typography.extraSmall = {
-  fontWeight: '400',
-  color: '#fff',
-  [theme.breakpoints.up('md')]: {
-    fontSize: '1rem',
+  fontWeight: "400",
+  color: "#fff",
+  [theme.breakpoints.up("md")]: {
+    fontSize: "1rem",
   },
-  [theme.breakpoints.only('md')]: {
-    fontSize: '0.9rem',
+  [theme.breakpoints.only("md")]: {
+    fontSize: "0.9rem",
   },
-  [theme.breakpoints.down('sm')]: {
-    fontSize: '0.9rem',
+  [theme.breakpoints.down("sm")]: {
+    fontSize: "0.9rem",
   },
-}
+};
 
 theme.typography.normal = {
-  fontWeight: '400',
-  color: '#000',
-  [theme.breakpoints.up('md')]: {
-    fontSize: '1rem',
+  fontWeight: "400",
+  color: "#000",
+  [theme.breakpoints.up("md")]: {
+    fontSize: "1rem",
   },
-  [theme.breakpoints.only('md')]: {
-    fontSize: '0.9rem',
+  [theme.breakpoints.only("md")]: {
+    fontSize: "0.9rem",
   },
-  [theme.breakpoints.down('sm')]: {
-    fontSize: '0.9rem',
+  [theme.breakpoints.down("sm")]: {
+    fontSize: "0.9rem",
   },
-}
+};
 
 theme.typography.aboutNormal = {
-  fontWeight: '400',
-  color: '#000',
-  [theme.breakpoints.up('md')]: {
-    fontSize: '1.2rem',
+  fontWeight: "400",
+  color: "#000",
+  [theme.breakpoints.up("md")]: {
+    fontSize: "1.2rem",
   },
-  [theme.breakpoints.only('md')]: {
-    fontSize: '1rem',
+  [theme.breakpoints.only("md")]: {
+    fontSize: "1rem",
   },
-  [theme.breakpoints.down('sm')]: {
-    fontSize: '1rem',
+  [theme.breakpoints.down("sm")]: {
+    fontSize: "1rem",
   },
-}
+};
 
-export default theme
+export default theme;
